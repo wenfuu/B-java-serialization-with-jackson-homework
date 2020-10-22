54da6d959329d835e72409aac54bd7a57001a659
Wen Fu <wen.fu@thoughtworks.com>
2020-10-22 12:07:32 +0800

    problem 2

--- src/main/java/com/thoughtworks/capability/gtb/vo/PersonVo.java
+++ src/main/java/com/thoughtworks/capability/gtb/vo/PersonVo.java
@@ -1,5 +1,10 @@
 package com.thoughtworks.capability.gtb.vo;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSetter;
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,8 +14,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class PersonVo {
 
-  private String id;
-  private Integer age;
-  private String name;
-  private String hobby;
+    private String id;
+    private Integer age;
+    private String name;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String hobby;
+
+    public Integer getAge() {
+        if (age == null) {
+            return 0;
+        } else {
+            return this.age;
+        }
+    }
 }
