f4b14e9d69f3c4e83336dd9fb667f45982e13d82
Wen Fu <wen.fu@thoughtworks.com>
2020-10-22 11:36:12 +0800

    problem 1

--- src/main/java/com/thoughtworks/capability/gtb/vo/EventType.java
+++ src/main/java/com/thoughtworks/capability/gtb/vo/EventType.java
@@ -1,8 +1,10 @@
 package com.thoughtworks.capability.gtb.vo;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum EventType {
   UPLOAD("U"), DOWNLOAD("D");
-
+  @JsonValue
   private String code;
 
   EventType(String code) {
--- src/main/java/com/thoughtworks/capability/gtb/vo/EventVo.java
+++ src/main/java/com/thoughtworks/capability/gtb/vo/EventVo.java
@@ -1,9 +1,14 @@
 package com.thoughtworks.capability.gtb.vo;
 
 import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Data
 @NoArgsConstructor
@@ -13,6 +18,8 @@ public class EventVo {
   private String id;
   private String name;
   private EventType type;
+  @JsonFormat(shape = JsonFormat.Shape.NUMBER)
   private Date time;
+  @JsonUnwrapped
   private UserVo user;
 }
